baa8fef258352ba72a3106fee6238e554cfe4fec
t.hallberg84 <t.hallberg84@gmail.com>
2025-04-06 14:09:39 +0200

    justerade

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
   return (
     <div>
       <Navbar/>
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<Booking />} />
